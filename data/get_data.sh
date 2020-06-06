42a6f3d3dbb0d6fac94c67f57767514a1b79176c
Thamme Gowda <tgowdan@gmail.com>
2020-06-06 13:58:28 -0500

    update dataset selection

--- data/get_data.sh
+++ data/get_data.sh
@@ -18,18 +18,24 @@ done
 
 INP=dataset-selection.tsv
 OUT_DIR=datasets
+ERR_OUT=errouts
+N_JOBS=20
 
 [[ -f $INP ]] || exit_log 3 "$INP file not found"
 [[ -d $OUT_DIR ]] || mkdir -p $OUT_DIR
+[[ -d $ERR_OUT ]] || mkdir -p $ERR_OUT
 
 cat $INP | awk -F '\t' 'NR > 1 && NF >= 4'  | awkg -F '\t' "
 lang, train, dev, test = R[:4]
-if train:
+out_dir=f'${OUT_DIR}/{lang}-eng'
+
+if train and not Path(f'{out_dir}/mtdata.signature.txt').exists():
    train = ' '.join([t.strip() for t in train.split(',')])
-   cmd = f'mtdata get -l {lang}-eng -tr {train} ' + f\" -o ${OUT_DIR}/{lang}-eng \"
+   cmd = f'mtdata get -l {lang}-eng -tr {train} -o {out_dir}'
    heldout = (test or '').split(',') + (dev or '').split(',')
    heldout = ' '.join(h.strip() for h in heldout if h.strip())
    if heldout:
       cmd += f' -ts {heldout}'
-   print(cmd)
-"
+   print(cmd + f' &> ${ERR_OUT}/{lang}')
+" 
+# |  tail -$N_JOBS | parallel -j $N_JOBS 
